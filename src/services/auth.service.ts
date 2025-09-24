4c70b279dec98f17620476ea1b43240094416adc
agusscript <agus.sanchez.240@gmail.com>
2025-09-23 22:42:03 -0300

    Refactor auth service to use axios

---
+++ src/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { ISignInResponse, ISignUpResponse, ISignUpFormValues, ISignInFormValues } from "@/src/interfaces/ISignupFormValues";
+import { ApiRequestConfig, apiService } from "@/src/services/api.service";
+
+class AuthService {
+  async signIn(values: ISignInFormValues, config?: ApiRequestConfig): Promise<ISignInResponse> {
+    return await apiService.post<ISignInResponse>(
+      "/auth/sign-in",
+      values,
+      config
+    );
+  }
+
+  async signUp(values: ISignUpFormValues, config?: ApiRequestConfig): Promise<ISignUpResponse> {
+    return await apiService.post<ISignUpResponse>(
+      "/auth/sign-up",
+      values,
+      config
+    );
+  }
+}
+
+export const authService = new AuthService();
