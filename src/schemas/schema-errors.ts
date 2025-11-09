a4d3c8074ef97b25fc9660c2a7dbd3765326569a
agusscript <agus.sanchez.240@gmail.com>
2025-11-08 22:33:07 -0300

    Change schema-errors language to spanish

--- src/schemas/schema-errors.ts
+++ src/schemas/schema-errors.ts
@@ -1,12 +1,10 @@
-export const NAME_REQUIRED = "Name is required";
-export const EMAIL_REQUIRED = "Please enter a valid email";
-export const EMAIL_INVALID = "Email must be a valid e-mail";
-export const PASSWORD_REQUIRED = "Password is required";
-export const PASSWORD_MIN_LENGTH = "Password should have at least 8 characters";
-export const PASSWORD_MAX_LENGTH = "Password should have at most 50 characters";
-export const PASSWORD_LOWERCASE =
-  "Password must contain at least 1 lowercase letter";
-export const PASSWORD_UPPERCASE =
-  "Password must contain at least 1 uppercase letter";
-export const PASSWORD_NUMBER = "Password must contain at least 1 number";
-export const PASSWORD_SPECIAL = "Password must contain at least 1 symbol";
+export const NAME_REQUIRED = "El nombre es requerido";
+export const EMAIL_REQUIRED = "El correo electrónico es requerido";
+export const EMAIL_INVALID = "El correo electrónico debe ser válido";
+export const PASSWORD_REQUIRED = "La contraseña es requerida";
+export const PASSWORD_MIN_LENGTH = "La contraseña debe tener al menos 8 caracteres";
+export const PASSWORD_MAX_LENGTH = "La contraseña debe tener como máximo 50 caracteres";
+export const PASSWORD_LOWERCASE = "La contraseña debe tener al menos 1 letra minúscula";
+export const PASSWORD_UPPERCASE = "La contraseña debe tener al menos 1 letra mayúscula";
+export const PASSWORD_NUMBER = "La contraseña debe tener al menos 1 número";
+export const PASSWORD_SPECIAL = "La contraseña debe tener al menos 1 símbolo";
